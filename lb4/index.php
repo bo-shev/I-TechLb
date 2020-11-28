5769518c604c1fd20970a30effc9cc888758ac3b
bo-shev <56201331+bo-shev@users.noreply.github.com>
2020-11-28 15:51:09 +0200

    lb4

---
+++ lb4/index.php
@@ -0,0 +1,21 @@
+<body>
+Тест 1:
+<?php
+echo "Hello, World!<br>"
+?>
+Тест 2:
+<?php echo 2+2 ?>
+
+
+<br>
+<form action = "vote.php" method="POST">
+<b>Яка ваша улюблена мова програмування?</b><p>
+<input type="Radio" name=otvet value=0 checked>С++<br>
+<input type="Radio" name=otvet value=1>PHP<br>
+<input type="Radio" name=otvet value=2>Java<p>
+<input type="Submit" name=vote value=" Голосовать! "><p>
+</form>
+
+
+
+</body>
